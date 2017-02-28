Attempted fixing rotation bug in new propagation scheme

code/tools/lenslet.py
@@ -147,7 +147,11 @@ def Lenslets(par, imageplane, lam, allweights,kernels,locations,lensletplane):
                 dispersion = par.npixperdlam*par.R*(lam*1000.-par.FWHMlam)/par.FWHMlam
 #                 if i==nx//2 and j==nx//2: print(dispersion)
                 coef = initcoef(order, scale=par.pitch/par.pixsize, phi=par.philens, x0=dispersion, y0=0)
-                sx, sy = transform(i-nx//2, j-nx//2, order, coef)
+                theta = np.arctan2(jcoord,icoord)
+                r = np.sqrt(icoord**2 + jcoord**2)
+                ilensx = r*np.cos(theta-par.philens)
+                ilensy = r*np.sin(theta-par.philens)
+                sx, sy = transform(ilensx, ilensy, order, coef)
                 sx+=par.npix//2
                 sy+=par.npix//2
 #                 if i==nx//2+1 and j==nx//2+1: print( sx, sy)